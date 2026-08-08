4a0d7644bbd56b113603662bf1ed2dfe9a51a3b3
Aakansha Singh <aakansha25csu001@ncuindia.edu>
2026-08-08 16:52:41 +0530

    add gradingsys program to week2

---
+++ week2/gradingsys.java
@@ -0,0 +1,27 @@
+package week2;
+import java.util.Scanner;
+public class gradingsys {
+public static void main (String[] args){
+   Scanner sc=new Scanner(System.in);
+    System.out.print("ENTER YOUR MARKS:");
+    int marks=sc.nextInt();
+    if(marks>=80 && marks<=100)
+        {
+        System.out.println("DISTINCTION");
+    }
+    else if(marks>=70 && marks<=79)
+        {
+        System.out.println("MERIT");
+    }
+    else if(marks>=50 && marks<=69){
+        System.out.println("PASS");
+    }else  if(marks>=0 && marks<=49){
+        System.out.println("FAIL");
+    }
+    else{
+        System.out.println("INVALID");
+    }
+    }
+   }
+
+
