8b408b89964b98a3031a837b7977d801f54b2e0d
Aakansha Singh <aakansha25csu001@ncuindia.edu>
2026-08-08 15:54:57 +0530

    this is my changes

---
+++ leapyear.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class leapyear{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a year:");
+        int year =sc.nextInt();
+        boolean leap=true;
+        if(year%400==0||(year%4==0 && year%100!=0))
+            {
+            leap=true;
+        } 
+    else {
+        leap=false;
+    }
+ System.out.println ("Leap Year:"+leap);
+}
+}
