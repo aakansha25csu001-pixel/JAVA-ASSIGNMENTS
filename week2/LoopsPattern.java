1b11ae8ac63e01942e6545ee3ac66167bf27d209
Aakansha Singh <aakansha25csu001@ncuindia.edu>
2026-08-08 21:10:45 +0530

    LOOPS

---
+++ week2/LoopsPattern.java
@@ -0,0 +1,99 @@
+// PATTERN A
+// package week2;
+// import java.util.Scanner;
+// public class LoopsPattern  {
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter size:");
+//         int size=sc.nextInt();
+//         for(int i=1;i<=size;i++){
+//             for(int j=1;j<=size;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println("");
+
+//         }
+//     }
+// }
+
+
+// PATTERN B
+// package week2;
+// import java.util.Scanner;
+// public class LoopsPattern  {
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter size:");
+//         int size=sc.nextInt();
+//         for(int i=1;i<=size;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println("");
+
+//         }
+//     }
+// }
+
+
+// PATTERN C
+// package week2;
+// import java.util.Scanner;
+// public class LoopsPattern  {
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter size:");
+//         int size=sc.nextInt();
+//         for(int i=1;i<=size;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j+" ");
+//             }
+//             System.out.println("");
+
+//         }
+//     }
+// }
+       
+// PATTERN D
+// package week2;
+// import java.util.Scanner;
+// public class LoopsPattern  {
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter size:");
+//         int size=sc.nextInt();
+        
+//         for(int i=size;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(" *");
+                
+//             }
+//             System.out.println("");
+
+//         }
+//     }
+// }         
+
+// PATTERN E
+// package week2;
+// import java.util.Scanner;
+// public class LoopsPattern  {
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter size:");
+//         int size=sc.nextInt();
+        
+//         for(int i=1;i<=size;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(i+" ");
+                
+//             }
+//             System.out.println("");
+
+//         }
+//     }
+// }
+
+
+
+
